44ecfa95a4edc2cc8255fa0a0b4ffb7aa8d1ce10
Mohini Pandey <mohini.pandey@numerator.com>
2021-03-04 13:55:28 +0530

    reduced sleep time

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  while(true) {
-    console.log('Containers rule!');
-    await sleep(5000);
+  while (true) {
+    console.log("Containers rule!");
+    await sleep(500);
   }
 }
 
